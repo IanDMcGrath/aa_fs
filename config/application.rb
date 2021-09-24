819177c5137fa83821a3b6400340f3607b333a69
IanDMcGrath <ian92415@gmail.com>
2021-09-24 13:21:00 -0700

    initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Artcoag
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # suggested for heroku push oh stack overflow # fix for "precompiling assets failed"
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
