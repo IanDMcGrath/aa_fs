12a4c7ab7b62f3ff889940938a5a1a6fd22e2bbc
IanDMcGrath <ian92415@gmail.com>
2021-10-09 09:47:16 -0700

    added tags medium seeds

--- db/seeds/5_tagging_seeds.rb
+++ db/seeds/5_tagging_seeds.rb
@@ -23,6 +23,7 @@ Tagging.create!(tag_id: 10, taggable_id: 6, taggable_type: "Art")
 Tagging.create!(tag_id: 3, taggable_id: 6, taggable_type: "Art")
 
 Tagging.create!(tag_id: 1, taggable_id: 7, taggable_type: "Art")
+Tagging.create!(tag_id: 3, taggable_id: 7, taggable_type: "Art")
 Tagging.create!(tag_id: 9, taggable_id: 7, taggable_type: "Art")
 
 Tagging.create!(tag_id: 2, taggable_id: 8, taggable_type: "Art")
@@ -37,3 +38,73 @@ Tagging.create!(tag_id: 7, taggable_id: 11, taggable_type: "Art")
 Tagging.create!(tag_id: 11, taggable_id: 11, taggable_type: "Art")
 
 Tagging.create!(tag_id: 2, taggable_id: 12, taggable_type: "Art")
+Tagging.create!(tag_id: 3, taggable_id: 12, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 13, taggable_type: "Art")
+Tagging.create!(tag_id: 8, taggable_id: 13, taggable_type: "Art")
+Tagging.create!(tag_id: 11, taggable_id: 13, taggable_type: "Art")
+
+Tagging.create!(tag_id: 1, taggable_id: 14, taggable_type: "Art")
+Tagging.create!(tag_id: 2, taggable_id: 14, taggable_type: "Art")
+
+Tagging.create!(tag_id: 10, taggable_id: 15, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 16, taggable_type: "Art")
+Tagging.create!(tag_id: 3, taggable_id: 16, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 17, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 18, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 19, taggable_type: "Art")
+Tagging.create!(tag_id: 3, taggable_id: 19, taggable_type: "Art")
+Tagging.create!(tag_id: 10, taggable_id: 19, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 20, taggable_type: "Art")
+Tagging.create!(tag_id: 3, taggable_id: 20, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 21, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 22, taggable_type: "Art")
+Tagging.create!(tag_id: 9, taggable_id: 22, taggable_type: "Art")
+Tagging.create!(tag_id: 10, taggable_id: 22, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 23, taggable_type: "Art")
+Tagging.create!(tag_id: 9, taggable_id: 23, taggable_type: "Art")
+Tagging.create!(tag_id: 10, taggable_id: 23, taggable_type: "Art")
+
+Tagging.create!(tag_id: 1, taggable_id: 24, taggable_type: "Art")
+Tagging.create!(tag_id: 8, taggable_id: 24, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 25, taggable_type: "Art")
+Tagging.create!(tag_id: 3, taggable_id: 25, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 26, taggable_type: "Art")
+Tagging.create!(tag_id: 10, taggable_id: 26, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 27, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 28, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 29, taggable_type: "Art")
+Tagging.create!(tag_id: 6, taggable_id: 29, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 30, taggable_type: "Art")
+Tagging.create!(tag_id: 6, taggable_id: 30, taggable_type: "Art")
+
+Tagging.create!(tag_id: 1, taggable_id: 31, taggable_type: "Art")
+Tagging.create!(tag_id: 8, taggable_id: 31, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 32, taggable_type: "Art")
+
+Tagging.create!(tag_id: 1, taggable_id: 33, taggable_type: "Art")
+Tagging.create!(tag_id: 7, taggable_id: 33, taggable_type: "Art")
+
+Tagging.create!(tag_id: 1, taggable_id: 34, taggable_type: "Art")
+Tagging.create!(tag_id: 8, taggable_id: 34, taggable_type: "Art")
+Tagging.create!(tag_id: 9, taggable_id: 34, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 35, taggable_type: "Art")
+Tagging.create!(tag_id: 3, taggable_id: 35, taggable_type: "Art")
+
+Tagging.create!(tag_id: 2, taggable_id: 36, taggable_type: "Art")
