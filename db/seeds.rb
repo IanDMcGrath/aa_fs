8b60058fa32ea01c29e52ec2dd2d23171787e6b6
IanDMcGrath <ian92415@gmail.com>
2021-11-09 18:10:49 -0800

    reverted seed file to use all seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,13 @@
 
 
 
-# puts "seeds.rb: Starting Seeds:"
-# Dir[File.join(Rails.root, 'db', 'seeds/','*.rb')].sort.each do |seed|
-#   puts "new seed file found..."
-#   load seed
-#   puts 'seeds loaded'
-# end
-# puts 'seeding complete!'
+puts "seeds.rb: Starting Seeds:"
+Dir[File.join(Rails.root, 'db', 'seeds/','*.rb')].sort.each do |seed|
+  puts "new seed file found..."
+  load seed
+  puts 'seeds loaded'
+end
+puts 'seeding complete!'
 
 # user = User.create!(username:"DemoUser", email:"demoemail", password:"password", work:"Artist at ___")
 # file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0003_Layer-0.png')
@@ -31,74 +31,74 @@
 
 
 
-avatar = Avatar.create!(user_id: 1)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0000_Layer-4.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0000_Layer-4.png')
-  file = nil
-end
-avatar = nil
-
-avatar = Avatar.create!(user_id: 2)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0001_Layer-3.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0001_Layer-3.png')
-  file = nil
-end
-avatar = nil
-
-avatar = Avatar.create!(user_id: 3)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0003_Layer-1.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0003_Layer-1.png')
-  file = nil
-end
-avatar = nil
-
-avatar = Avatar.create!(user_id: 4)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0004_Layer-5.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0004_Layer-5.png')
-  file = nil
-end
-avatar = nil
-
-avatar = Avatar.create!(user_id: 5)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0006_Layer-7.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0006_Layer-7.png')
-  file = nil
-end
-avatar = nil
-
-avatar = Avatar.create!(user_id: 6)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0005_Layer-6.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0005_Layer-6.png')
-  file = nil
-end
-avatar = nil
-
-avatar = Avatar.create!(user_id: 7)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0007_Layer-8.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0007_Layer-8.png')
-  file = nil
-end
-avatar = nil
-
-avatar = Avatar.create!(user_id: 8)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0007_Layer-8.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0007_Layer-8.png')
-  file = nil
-end
-avatar = nil
-
-avatar = Avatar.create!(user_id: 9)
-file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0003_Layer-0.png')
-if file
-  avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0003_Layer-0.png')
-  file = nil
-end
-avatar = nil
+# avatar = Avatar.create!(user_id: 1)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0000_Layer-4.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0000_Layer-4.png')
+#   file = nil
+# end
+# avatar = nil
+#
+# avatar = Avatar.create!(user_id: 2)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0001_Layer-3.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0001_Layer-3.png')
+#   file = nil
+# end
+# avatar = nil
+#
+# avatar = Avatar.create!(user_id: 3)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0003_Layer-1.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0003_Layer-1.png')
+#   file = nil
+# end
+# avatar = nil
+#
+# avatar = Avatar.create!(user_id: 4)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0004_Layer-5.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0004_Layer-5.png')
+#   file = nil
+# end
+# avatar = nil
+#
+# avatar = Avatar.create!(user_id: 5)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0006_Layer-7.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0006_Layer-7.png')
+#   file = nil
+# end
+# avatar = nil
+#
+# avatar = Avatar.create!(user_id: 6)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0005_Layer-6.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0005_Layer-6.png')
+#   file = nil
+# end
+# avatar = nil
+#
+# avatar = Avatar.create!(user_id: 7)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0007_Layer-8.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0007_Layer-8.png')
+#   file = nil
+# end
+# avatar = nil
+#
+# avatar = Avatar.create!(user_id: 8)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0007_Layer-8.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0007_Layer-8.png')
+#   file = nil
+# end
+# avatar = nil
+#
+# avatar = Avatar.create!(user_id: 9)
+# file = open('https://artcoag-seeds.s3.us-west-1.amazonaws.com/avatars/fsp_icons_0003_Layer-0.png')
+# if file
+#   avatar.avatar_img.attach(io: file, filename: 'fsp_icons_0003_Layer-0.png')
+#   file = nil
+# end
+# avatar = nil
